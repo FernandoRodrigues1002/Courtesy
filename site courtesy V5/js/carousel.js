242c409b1e740aefe64c671b39c9501b0d70f57c
nando <103220295+FernandoRodrigues1002@users.noreply.github.com>
2024-12-18 16:39:13 -0300

    Add files via upload

---
+++ site courtesy V5/js/carousel.js
@@ -0,0 +1,33 @@
+$('.carousel').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  responsive: [
+    {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false
+        }
+      },
+    {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false
+        }
+      },
+    {
+        breakpoint: 550,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false
+        }
+      }
+    ]
+});
